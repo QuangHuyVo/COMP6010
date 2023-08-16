db53088d9c5e596482e306b401f223771c56ab72
QuangHuyVo <quanghuy.vo@students.mq.edu.au>
2023-08-16 23:24:45 +1000

    Note

---
+++ Week 1 /Week 1.py
@@ -0,0 +1,138 @@
+# Week 1
+
+ # do something
+
+print (5)
+
+ # store something in a variable
+
+day = 5 #integer
+
+print (day) 
+
+ # updating a variable
+
+day = day + 2 # set day equal to day plus 2
+
+print (day)
+
+isWednesday = False #boolean
+
+today = "Thursday" 
+
+today = 4
+
+print (today == 3) # prints False because today == 4 (to check value) 
+
+ # Think about ATM Pins
+
+carlPin = 1234
+
+yourGuess = 5674
+
+yourotherguess = 3214
+
+print(carlPin == yourGuess) #False
+
+print(carlPin == carlPin) #True
+
+yourGuess = 1234
+
+print(carlPin == yourGuess) #True
+
+""""
+Data types
+
+boolean → True, False
+
+Integers → whole numbers
+
+Strings → Characters in quotes “  “, ‘c’
+
+floating point numbers → integer.integer
+
+- Operators
+
++
+
+-
+
+/
+
+*
+
+8 / 5 = 1.6 (floating point diviser)
+
+8 // 5 = 1 (just the result, not the decimal)
+
+8 % 5 = 3 (remainder / modulo) - lấy số dư
+
+is a number odd?’
+
+x % 2 == 1
+
+print(”Carl” + 5) ⇒ syntax
+
+⇒ print(”Carl” + str(5))
+
+#Print
+
+⇒ String “Carl”
+
+⇒ concatentate
+
+⇒ number 5 converted to a string first
+
+- Comparisons… ⇒ True or False
+
+==
+
+≤
+
+≠ (not equal to ! =)
+
+≥
+
+<
+
+>
+
+- Logical operators
+
+and 
+
+or
+
+not 
+
+in
+
+ # type casting (change variable types)
+
+print (type(5)) ⇒ int
+
+print (type(8.3)) ⇒ floating
+
+ # order of operations
+
+BODMAS
+
+brackets 
+
+ordinals (exponents) 5^3
+
+Mult Division
+
+Add Subtract
+
+- Number system
+
+binary (base 2): 1,0 
+
+octal (base 8): 0,1,2,3,4,5,6,7
+
+decimal (base 10): 0→9
+
+hexadecimal (base 16)
+
+"""
---
+++ Week 1 /week1-examples/output2.py
@@ -0,0 +1,7 @@
+print('a book titled "Python Tutorial"')
+print("a book titled 'Python Tutorial'")
+print('''
+How are you?
+This is our first class to learn Python
+Wish you all enjoy this unit.''')
+#print('sth")
